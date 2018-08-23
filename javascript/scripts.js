4982d6e278d97e325df0bcfff387500ea6baffb5
Rodrigo Golfeto <rgolfeto@gmail.com>
2018-08-22 22:30:23 -0400

    Recorte - Formulários - Cadastro Pessoa e Anúncio

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,4 +2,11 @@ $(document).ready(function() {
     $(".hp-select").select2({
     	placeholder: "Tipo do imóvel"
 	});
+
+    $( "#campo_pessoa_juridica").click(function() {
+    	$('.hp-grupo-pessoa-fisica').removeClass('d-none')
+	});
+	$( "#campo_pessoa_fisica").click(function() {
+    	$('.hp-grupo-pessoa-fisica').addClass('d-none')
+	});
 });
